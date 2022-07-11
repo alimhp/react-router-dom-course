077d0d01420bb987f831f17d84b3d407b5986b24
alimhp <alimohamadipourr@gmail.com>
2022-07-11 11:12:25 -0700

    creat navigation

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import AboutUs from "./pages/AboutUs";
 import HomePage from "./pages/HomePage";
+import Navigation from "./component/Navigation";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navigation />
+
       <Route path="/" exact={true} component={HomePage}></Route>
       <Route path="/about-us" component={AboutUs}></Route>
     </BrowserRouter>
