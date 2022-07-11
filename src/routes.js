cfcc3e621d694dbf5690f181914590b5a741a27f
alimhp <alimohamadipourr@gmail.com>
2022-07-11 13:21:53 -0700

    Concept of params and dynamic routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
+import Blog from "./component/Blog";
 import NotFound from "./component/NotFound";
 import AboutUs from "./pages/AboutUs";
+import Blogs from "./pages/Blogs";
 import HomePage from "./pages/HomePage";
 import Profile from "./pages/Profile";
 
 const route = [
-  { path: "/", component: HomePage, exact: true },
+  { path: "/blogs/:id", component: Blog },
   { path: "/about-us", component: AboutUs },
   { path: "/profile", component: Profile },
+  { path: "/blogs", component: Blogs },
+  { path: "/", component: HomePage, exact: true },
   { path: "*", component: NotFound },
 ];
 export default route;
