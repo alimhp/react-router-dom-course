3fa9b49d89fee2188b7dbcff3a24f5d98d0a6141
alimhp <alimohamadipourr@gmail.com>
2022-07-11 11:06:18 -0700

    Develop general part of project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
+import AboutUs from "./pages/AboutUs";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={HomePage}></Route>
+      <Route path="/" exact={true} component={HomePage}></Route>
+      <Route path="/about-us" component={AboutUs}></Route>
     </BrowserRouter>
   );
 }
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,10 @@
+const AboutUs = () => {
+  return (
+    <>
+      <p>about us</p>
+      <a href="/">go to home page</a>
+    </>
+  );
+};
+
+export default AboutUs;
